c594dd37203b243cd24f11bbd38a8fb5395bcf54
adamijak <adamik.jakub72@gmail.com>
2022-04-23 15:48:59 +0200

    feat: longer reports

--- main/device_client.c
+++ main/device_client.c
@@ -66,7 +66,7 @@ void device_reporter_task(void *param)
         IoTHubMessage_Destroy(message);
         json_free_serialized_string(message_string);
 
-        ThreadAPI_Sleep(5 * 60000);
+        ThreadAPI_Sleep(60 * 60000);
     }
 }
 
